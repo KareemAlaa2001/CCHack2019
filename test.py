6720f4fb150ab151a963473609ae6ddb3937353b
deb1g19 <56115125+deb1g19@users.noreply.github.com>
2019-10-12 18:45:59 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 import time
 import math
-
+import VL53L1X
 from matrix11x7 import Matrix11x7
 
 matrix11x7 = Matrix11x7()
+tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
+tof.open()
+tof.start_ranging(3)
+
+def convertDistanceToBrightness(distance):
+    brightness = (distance/(400*10))
+    return brightness
 
 # Avoid retina-searage!
-for x in range(0, matrix11x7.width):
-        for y in range(0, matrix11x7.height):   
-            matrix11x7.pixel(x, y, 1)
-            
-matrix11x7.show()
+def setBrightness(value):
+    for x in range(0, matrix11x7.width):
+            for y in range(0, matrix11x7.height):   
+                matrix11x7.pixel(x, y, value)
+        
+while True:
+    distance_in_mm = tof.get_distance()
+    setBrightness(convertDistanceToBrightness(distance_in_mm))
+    matrix11x7.show()
